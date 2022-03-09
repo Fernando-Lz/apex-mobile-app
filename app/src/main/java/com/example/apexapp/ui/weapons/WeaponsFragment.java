b4884c40df6ab13d1f047b5f2830d6eafbd55d44
Fernando <flopezlopez6@outlook.com>
2022-03-09 16:03:35 -0600

    Weapons layout added

--- app/src/main/java/com/example/apexapp/ui/weapons/WeaponsFragment.java
+++ app/src/main/java/com/example/apexapp/ui/weapons/WeaponsFragment.java
@@ -24,8 +24,8 @@ public class WeaponsFragment extends Fragment{
         binding = FragmentWeaponsBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textWeapons;
-        legendsViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+//        final TextView textView = binding.textWeapons;
+//        legendsViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
     }
 
