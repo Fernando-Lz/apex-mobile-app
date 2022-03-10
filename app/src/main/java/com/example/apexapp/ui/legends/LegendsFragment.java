e545b8ca60de41eabcc84898e7ecfa2da2a54817
Fernando Lopez <flopezlopez6@outlook.com>
2022-03-09 22:38:25 -0600

    Legends fragment layout finished

--- app/src/main/java/com/example/apexapp/ui/legends/LegendsFragment.java
+++ app/src/main/java/com/example/apexapp/ui/legends/LegendsFragment.java
@@ -24,8 +24,8 @@ public class LegendsFragment extends Fragment {
         binding = FragmentLegendsBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textLegends;
-        legendsViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+//        final TextView textView = binding.textLegends;
+//        legendsViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
     }
 
