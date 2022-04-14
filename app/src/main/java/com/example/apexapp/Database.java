35db4c7c99f05d4b689c1d54688f3e660dca8561
Fernando <flopezlopez6@outlook.com>
2022-04-13 22:20:13 -0500

    Sign up user to db added

--- app/src/main/java/com/example/apexapp/Database.java
+++ app/src/main/java/com/example/apexapp/Database.java
@@ -1,12 +1,32 @@
 package com.example.apexapp;
 
-import java.util.LinkedList;
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class Database {
-
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
 
     public boolean registerUser(String username, String password) {
-        System.out.println("Hola");
+        Map<String, Object> data = new HashMap<>();
+        data.put("username", username);
+        data.put("password", password);
+        data.put("platform", "PC");
+        db.collection("user").document().set(data).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+             if (task.isSuccessful()){
+                 System.out.println("Usuario registrado correctamente");
+             } else {
+                 System.out.println("Usuario no pudo ser registrado");
+             }
+            }
+        });
         return true;
     }
     /*
